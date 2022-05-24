839e1b4bb17f8f0a62b9a15bf9487333309cf47b
David Díez <ddiezgon91@gmail.com>
2022-05-24 18:58:15 +0200

    inserts y más imágenes

---
+++ datos_bbdd_laravending.sql
@@ -0,0 +1,17 @@
+USE laravending;
+
+INSERT INTO users (`name`, `email`, `password`, `saldo`) VALUES ('user_1', 'user1@gmail.com', 'password', 100);
+INSERT INTO users (`name`, `email`, `password`, `saldo`) VALUES ('user_2', 'user2@gmail.com', 'password', 100);
+
+INSERT INTO products (`name`, `pais`, `precio`, `kcal1`, `kcal100`, `grasas`, `carbohidratos`, `proteinas`) VALUES ('bocadillo', 'Ucrania', 3, 200, 100, 17, 35, 53);
+INSERT INTO products (`name`, `pais`, `precio`, `kcal1`, `kcal100`, `grasas`, `carbohidratos`, `proteinas`) VALUES ('cafe', 'Italia', 1, 100, 500, 2, 0, 30);
+INSERT INTO products (`name`, `pais`, `precio`, `kcal1`, `kcal100`, `grasas`, `carbohidratos`, `proteinas`) VALUES ('cerveza', 'Estados Unidos', 2, 300, 400, 60, 60, 20);
+INSERT INTO products (`name`, `pais`, `precio`, `kcal1`, `kcal100`, `grasas`, `carbohidratos`, `proteinas`) VALUES ('chuches', 'Francia', 1, 1200, 4000, 40, 40, 40);
+INSERT INTO products (`name`, `pais`, `precio`, `kcal1`, `kcal100`, `grasas`, `carbohidratos`, `proteinas`) VALUES ('donut de chocolate', 'Inglaterra', 2, 650, 1200, 37, 50, 4);
+INSERT INTO products (`name`, `pais`, `precio`, `kcal1`, `kcal100`, `grasas`, `carbohidratos`, `proteinas`) VALUES ('doritos', 'Afganistán', 3, 5550, 4350, 34, 34, 0);
+INSERT INTO products (`name`, `pais`, `precio`, `kcal1`, `kcal100`, `grasas`, `carbohidratos`, `proteinas`) VALUES ('sobre de cartas Magic', 'Estados Unidos', 4, 0, 0, 0, 0, 0);
+INSERT INTO products (`name`, `pais`, `precio`, `kcal1`, `kcal100`, `grasas`, `carbohidratos`, `proteinas`) VALUES ('palomitas', 'España', 3, 930, 150, 52, 21, 5);
+INSERT INTO products (`name`, `pais`, `precio`, `kcal1`, `kcal100`, `grasas`, `carbohidratos`, `proteinas`) VALUES ('filipinos', 'España', 4, 2100, 1700, 51, 42, 9);
+INSERT INTO products (`name`, `pais`, `precio`, `kcal1`, `kcal100`, `grasas`, `carbohidratos`, `proteinas`) VALUES ('maltesers', 'Francia', 2, 3205, 2400, 30, 42, 13);
+INSERT INTO products (`name`, `pais`, `precio`, `kcal1`, `kcal100`, `grasas`, `carbohidratos`, `proteinas`) VALUES ('mars', 'Estados Unidos', 2, 1700, 1350, 35, 42, 3);
+INSERT INTO products (`name`, `pais`, `precio`, `kcal1`, `kcal100`, `grasas`, `carbohidratos`, `proteinas`) VALUES ('galletas oreo', 'Alemania', 2, 3210, 1500, 83, 62, 12);
